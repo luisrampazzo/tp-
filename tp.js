00cbd40c8d18e85a8df7f5ce48b3971715d755a7
luisrampazzo <saborsantafe@gmail.com>
2023-04-17 17:44:25 -0300

    actualizacion 17-4
    paleta de coloresy ccs

---
+++ tp.js
@@ -0,0 +1,11 @@
+
+
+let nombre = document.getElementById("nombre");
+let empresa = document.getElementById("apellido");
+let direccion = document.getElementById("telefono");
+let telefono = document.getElementById("email");
+let mail = document.getElementById("comentario");
+
+console.log="hola"
+
+console.log("nombre");
